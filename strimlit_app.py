e4d62cc6d9df0ab2fb07ddf108a0257451c0c504
Rahul Shah <74894594+toretto17@users.noreply.github.com>
2023-11-14 16:15:51 +0530

    Update strimlit_app.py
    added images as well

--- strimlit_app.py
+++ strimlit_app.py
@@ -2,6 +2,7 @@ import streamlit as st
 
 st.title('Hii frineds, chai pilo')
 st.header('Breakfast Menu')
-st.text('Omega 3 & Blueberry Oatmeal')
-st.text('Kale, Spinach & Rocket Smoothie')
-st.text('Hard-Boiled Free-Range Egg')
+st.text('🥣 Omega 3 & Blueberry Oatmeal')
+st.text('🥗Kale, Spinach & Rocket Smoothie')
+st.text('🐔Hard-Boiled Free-Range Egg')
+st.text('🥑🍞Avocado Toast')
