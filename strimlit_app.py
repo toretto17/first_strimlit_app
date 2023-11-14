f431d3426d94fe119b2ad8304e6caa9b70911108
Rahul Shah <74894594+toretto17@users.noreply.github.com>
2023-11-14 16:28:44 +0530

    Update strimlit_app.py
    code changes

--- strimlit_app.py
+++ strimlit_app.py
@@ -11,6 +11,6 @@ st.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 my_fruit_list = my_fruit_list.set_index('Fruit')
-selected_fruits = st.multiselect("Pick some fruits:", list(my_fruit_list.index).['Avocado','Strawberries'])
+selected_fruits = st.multiselect("Pick some fruits:", list(my_fruit_list.index),['Avocado','Strawberries'])
 to_show = my_fruit_list.loc[selected_fruits]
 st.dataframe(to_show)
