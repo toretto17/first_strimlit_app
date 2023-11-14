f540761875e8aa198abf883a785df5c179fe090d
Rahul Shah <74894594+toretto17@users.noreply.github.com>
2023-11-14 16:18:42 +0530

    Update strimlit_app.py
    added pandas and displayed df

--- strimlit_app.py
+++ strimlit_app.py
@@ -1,4 +1,5 @@
 import streamlit as st
+import pandas
 
 st.title('Hii frineds, chai pilo')
 st.header('Breakfast Menu')
@@ -7,3 +8,6 @@ st.text('🥗Kale, Spinach & Rocket Smoothie')
 st.text('🐔Hard-Boiled Free-Range Egg')
 st.text('🥑🍞Avocado Toast')
 st.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+st.dataframe(my_fruit_list)
