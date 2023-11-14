9a522f6371f7af4b66e40905b1991aeef83bc2ab
Rahul Shah <74894594+toretto17@users.noreply.github.com>
2023-11-14 16:12:26 +0530

    Update strimlit_app.py
    added other texts

--- strimlit_app.py
+++ strimlit_app.py
@@ -1,3 +1,7 @@
 import streamlit as st
 
 st.title('Hii frineds, chai pilo')
+st.header('Breakfast Menu')
+st.text('Omega 3 & Blueberry Oatmeal')
+st.text('Kale, Spinach & Rocket Smoothie')
+st.text('Hard-Boiled Free-Range Egg')
