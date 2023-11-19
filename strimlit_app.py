7010396420827d0f94811fb485c073f2ed244856
Rahul Shah <74894594+toretto17@users.noreply.github.com>
2023-11-19 11:57:34 +0530

    Update strimlit_app.py
    code changes

--- strimlit_app.py
+++ strimlit_app.py
@@ -18,6 +18,11 @@ selected_fruits = st.multiselect("Pick some fruits:", list(my_fruit_list.index),
 to_show = my_fruit_list.loc[selected_fruits]
 st.dataframe(to_show)
 
+def get_fruityvice_data(this_fruit_choice):
+  fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + this_fruit_choice)
+  fruityvice_normalized = pd.json_normalize(fruityvice_response.json())
+  return fruityvice_normalized
+
 st.header("Fruityvice Fruit Advice!")
 try:
   fruit_choice = st.text_input('What fruit would you like information about?')
@@ -25,9 +30,8 @@ try:
     st.error('Please select a fruit to get information')
   else:
     st.write('The user entered ', fruit_choice)
-    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
-    fruityvice_normalized = pd.json_normalize(fruityvice_response.json())
-    st.dataframe(fruityvice_normalized)
+    back_from_function = get_fruityvice_data(fruit_choice)
+    st.dataframe(back_from_function)
 except URLError as e:
   st.stop()
 my_cnx = snowflake.connector.connect(**st.secrets["snowflake"])
