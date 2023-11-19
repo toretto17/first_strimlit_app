ed8bc14d7e36a311291c2cec522d89b499bb8c10
Rahul Shah <74894594+toretto17@users.noreply.github.com>
2023-11-19 11:53:01 +0530

    Update strimlit_app.py
    changes code

--- strimlit_app.py
+++ strimlit_app.py
@@ -18,16 +18,18 @@ selected_fruits = st.multiselect("Pick some fruits:", list(my_fruit_list.index),
 to_show = my_fruit_list.loc[selected_fruits]
 st.dataframe(to_show)
 
-
-fruit_choice = st.text_input('What fruit would you like information about?','Kiwi')
-st.write('The user entered ', fruit_choice)
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
 st.header("Fruityvice Fruit Advice!")
-fruityvice_normalized = pd.json_normalize(fruityvice_response.json())
-# write your own comment - what does this do?
-st.dataframe(fruityvice_normalized)
-
-st.stop()
+try:
+  fruit_choice = st.text_input('What fruit would you like information about?')
+  if not fruit_choice:
+    st.error('Please select a fruit to get information')
+  else:
+    st.write('The user entered ', fruit_choice)
+    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
+    fruityvice_normalized = pd.json_normalize(fruityvice_response.json())
+    st.dataframe(fruityvice_normalized)
+except URLError as e:
+  st.stop()
 my_cnx = snowflake.connector.connect(**st.secrets["snowflake"])
 my_cur = my_cnx.cursor()
 my_cur.execute("select * from fruit_load_list")
